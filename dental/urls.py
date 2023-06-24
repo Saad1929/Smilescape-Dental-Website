e852f329c16ad6ca6fcec5102ac7bf4c53cee81d
Saad1929 <saad2baig123@gmail.com>
2023-06-24 19:28:47 +0100

    URL Update

--- dental/urls.py
+++ dental/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+    path('dental/', include('website.urls'))
 ]
